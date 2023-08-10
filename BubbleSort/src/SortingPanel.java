8e6fb31f23aab20a17fd2b5f5026af7a24f321c3
anilghale1 <ghalepasang5@gmail.com>
2023-08-10 21:44:21 +0545

    updated sorted colour at end

--- BubbleSort/src/SortingPanel.java
+++ BubbleSort/src/SortingPanel.java
@@ -8,6 +8,8 @@ public class SortingPanel extends JPanel implements ActionListener {
     private static final long serialVersionUID = 1L;
     private Random random = new Random();
     private int[] array = new int[80];
+    private boolean[] sortedArray = new boolean[array.length];
+
     private int array_index;
     private int compare_index;
 
@@ -15,7 +17,6 @@ public class SortingPanel extends JPanel implements ActionListener {
     JButton reset = new JButton("Reset");
 
     private boolean isRunning;
-
     public SortingPanel() {
         this.array_index = 0;
         this.compare_index = Integer.MAX_VALUE;
@@ -36,6 +37,10 @@ public class SortingPanel extends JPanel implements ActionListener {
         start.addActionListener(this);
         reset.addActionListener(this);
 
+
+
+
+
     }
 
     public int[] getArray() {
@@ -50,17 +55,19 @@ public class SortingPanel extends JPanel implements ActionListener {
 
     public void sortOnlyOneItem() {
         if (array[compare_index] > array[compare_index + 1]) {
-
             int temp = array[compare_index];
             array[compare_index] = array[compare_index + 1];
             array[compare_index + 1] = temp;
+            sortedArray[compare_index] = false;  // Mark as unsorted
+            sortedArray[compare_index + 1] = false;  // Mark as unsorted
         }
         if ((compare_index + 1) >= (array.length - array_index - 1)) {
             array_index++;
             compare_index = 0;
-
+            sortedArray[array_index - 1] = true;  // Mark as sorted
         } else compare_index++;
     }
+
 
     public Boolean isSorted() {
         for (int i = 0; i < (array.length) - 1; i++) {
@@ -101,18 +108,22 @@ public class SortingPanel extends JPanel implements ActionListener {
         super.paintComponent(g);
         this.setBackground(Color.black);
 
-        for (int i = 0; i< array.length - 1; i++)
-        {
-            g.setColor(Color.cyan);
-            if (i==compare_index || i == compare_index + 1)
-            {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (sortedArray[i]) {
+                g.setColor(Color.green);  // Marked as sorted
+            } else if (i >= array.length - array_index - 1) {
+                g.setColor(Color.green);  // Right part of the array
+            } else if (i == compare_index || i == compare_index + 1) {
                 g.setColor(Color.red);
+            } else {
+                g.setColor(Color.cyan);
             }
-            g.drawRect(i*15, 600 - array[i],14, array[i]);
-            g.fillRect(i*15, 600 - array[i],14, array[i]);
-
+            g.drawRect(i * 15, 600 - array[i], 14, array[i]);
+            g.fillRect(i * 15, 600 - array[i], 14, array[i]);
         }
     }
+
+
 
 
     @Override
